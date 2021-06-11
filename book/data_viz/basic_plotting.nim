ea2a11ac8c9a5be1c96067669d0623c101060565
Vindaar <basti90@gmail.com>
2021-06-12 00:32:44 +0200

    add theme_opaque for white background, better contrast on dark themes

--- book/data_viz/basic_plotting.nim
+++ book/data_viz/basic_plotting.nim
@@ -17,6 +17,7 @@ nbCode:
   let df1 = seqsToDf(x1, y1)
   ggplot(df1, aes("x1", "y1")) +
     geom_line() +
+    theme_opaque() +
     ggsave("images/line_plot.png")
 
 nbImage("images/line_plot.png")
@@ -29,6 +30,7 @@ nbCode:
   let df2 = seqsToDf(x2, y2)
   ggplot(df2, aes("x2", "y2")) +
     geom_point() +
+    theme_opaque() +
     ggsave("images/scatter_plot.png")
 
 nbImage("images/scatter_plot.png")
