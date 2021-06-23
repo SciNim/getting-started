8daf1bc82b0c0e5a684b5ea567acbfbe98166ce4
Vindaar <basti90@gmail.com>
2021-06-23 12:18:01 +0200

    update ggplotnim plotting introduction

--- book/data_viz/plotting_data.nim
+++ book/data_viz/plotting_data.nim
@@ -36,6 +36,45 @@ This is because `ggplot2` is an implementation of the so called "grammar of grap
 It's the NixOS of plotting libraries. Tell it what you want and it gets it done for you,
 as long as you speak its language.
 
+## A motivating example
+
+Let's now consider a somewhat complicated plotting example. Using that we will look at why
+it is called a *grammar* of graphics.
+"""
+block:
+  nbCode:
+    ## ignore the dummy `df` here. This is to be able to compile the code (we throw away
+    ## the `ggplot` result as we don't call `ggsave`)
+    let df = seqsToDf({"Energy" : @[1], "Counts" : @[2], "Type" : @["background"]})
+    discard ggplot(df, aes("Energy", "Counts", fill = "Type", color = "Type")) +
+      geom_histogram(stat = "identity", position = "identity", alpha = some(0.5), hdKind = hdOutline) +
+      geom_point(binPosition = "center") +
+      geom_errorbar(data = df.filter(f{`Type` == "background"}),
+                    aes = aes(yMin = f{max(`Counts` - 1.0, 0.0)}, yMax = f{`Counts` + 1.0}),
+                    binPosition = "center") +
+      xlab("Energy [keV]") + ylab("#") +
+      ggtitle("A multi-layer plot of a histogram and scatter plot with error bars")
+nbText: """
+It may seem overwhelming. But it's actually simple and can be read from top to bottom.
+In words all this says is:
+
+"Create a plot from the input data frame `df` using column 'Energy' for the x axis, 'Counts'
+for the y axis and color the data (both outline `color` and fill color `fill`) based on the
+discrete entries of column 'Type'. With it draw:
+- a histogram without statistical computations (`stat = "identity"`, i.e. don't *compute* a histogram
+  but use the data as a continuous bar plot), draw them in identity position (where the data says,
+  no stacking of bars), add some alpha to the color and draw it as an outline.
+- a scatter plot in the center positions of each bin (`binPosition = "center"`), as the data
+  contains bin edges.
+- errorbars for all data of type 'background' (`data = df.filter(…)`), where the error bars range
+  from 'yMin' to 'yMax' for all points, also in center position.
+Finally, customize x (`xlab`) and y (`ylab`) labels and add a title (`ggtitle`)."
+
+The only thing we left out is the `ggsave` call, as we only have a dummy data frame here. We
+will now walk through the basic building blocks of every plot and then look at the above as
+an actual plot. After reading the next part looking at the plot above again should make it seem
+less dense already.
+
 ## The 3 (or 4) basic building blocks of a "ggplot" plot
 
 There are 3 (in some respect 4) major pieces that make up the basic syntax of *every single*
@@ -139,19 +178,89 @@ the mapping to more specific things to be drawn is natural. For a point plot the
 size and the color the point color. For a line it's line width and color and so on.
 
 This part of the ggplot construction might be the most "vague" at first. But with it we can
-now continue our construction. Assume our data frame `df` has columns "x" and "y" (continuous),
+now continue our construction. Assume our data frame `df` has columns "Energy" and "Counts" (continuous),
 "Type" (discrete).
 
 ```nim
-ggplot(df, aes("x", "y", fill = "Type", color = "Type"))
+ggplot(df, aes("Energy", "Counts", fill = "Type", color = "Type"))
 ```
 
-## plot something
+If you paid close attention to the plot example above, you will have noticed that for `yMin` and `yMax`
+we did not actually hand a column, but rather a `ggplotnim` formula. This is the main reason `aes` is
+a macro. You can hand *any* formula that references local variables or column references, or simply
+assign constant values (`aes(width = 5)` is perfectly valid). `ggplotnim` will compute the resulting
+values for you automatically before plotting.
 
-And plot something!
+### `geoms` - Geometric shapes to fill a plot
+
+Input data and aesthetics of course are not enough to actually draw a plot. So far we have only
+stated what part of the data to use and added a discrete classification by one column (fill
+and color the "Type" column).
+
+In a sense this has described our coordinate system so far (from the continuity or discreteness)
+we can determine the ranges / classes for each axis, same for the colors. Now we need to tell
+the plot what to apply this to.
+
+This is where the layering structure of `ggplot` actually becomes apparent, because now we
+will just list all kinds of `geoms` we wish to draw. The order we list them directly determines
+the order they are drawn in (the later ones are drawn on top of the former ones, which is important
+to remember for more busy plots).
+
+This is what all available `geom_*` procedures are for. They return `Geom` variant objects that mainly
+just store their kind and possibly some specific information required to draw them.
+
+The (currently) implemented geoms are as follows (with the required aesthetics listed):
+- `geom_point`: draw points for each `x`/`y`
+- `geom_line`: draw a line through all `x`/`y`
+- `geom_errorbar`: draw error bars from `xMin` to `xMax` or `yMin` to `yMax` at `x`/`y`
+- `geom_linerange`: draw lines from `xMin` to `xMax` or `yMin` to `yMax`
+- `geom_bar`: draw a *discrete* bar plot using the occurrences (default `stat = "count"`) of each
+   discrete value in `x` or the number of counts indicated in `y` (`stat = "identity"`)
+- `geom_histogram`: draw a *continuous* bar plot computing a histogram from continuous variable `x`
+   (default `stat = "bin"`) or draw continuous bars starting at `x` and the number of entries
+   indicated in `y` (`stat = "identity"`).
+- `geom_freqpoly`: same as `geom_histogram`, but connect bin centers by lines instead of drawing bars
+- `geom_tile`: draw discrete tiles at `x`/`y` (default position bottom left) with width `width` and height
+  `height` each. Tiles don't need to touch.
+- `geom_raster`: draw fully connected tiles at `x`/`y` of `width` and `height`. `width` and `height` must
+   be constant!
+- `geom_text`: draw text at `x`/`y` containing `text` (the `text` aesthetic)
+
+Here we stated mainly the *typical* (or default) use cases. All geoms take all arguments. That means
+you can also draw a histogram using points by applying the `stat = "bin"` argument. The difference
+is just in the defaults! Or in case of a `geom_histogram` call you can indicate that the `binPosition`
+should be `"center"` instead of the default `"left"` to have `x` indicate the bin centers.
+
+The possibilities are almost endless. You can combine any geom with (almost) any option and
+it *should just work* (few exceptions exist, e.g. `geom_raster` only draws fixed size tiles for performance).
+
+## Another look at *that* plot
+
+Let's look at the plot from above again. This time read the command with the gained knowledge and then
+see if the explanation in words makes more sense now.
 """
+nbCode:
+  import ggplotnim, random, sequtils
+  let df = seqsToDf({ "Energy" : cycle(linspace(0.0, 10.0, 25).toRawSeq, 2),
+                      "Counts" : concat(toSeq(0 ..< 25).mapIt(rand(10.0)),
+                                        toSeq(0 ..< 25).mapIt(rand(10).float)),
+                      "Type" : concat(newSeqWith(25, "background"),
+                                      newSeqWith(25, "candidates")) })
+  ggplot(df, aes("Energy", "Counts", fill = "Type", color = "Type")) +
+    geom_histogram(stat = "identity", position = "identity", alpha = some(0.5), hdKind = hdOutline) +
+    geom_point(binPosition = "center") +
+    geom_errorbar(data = df.filter(f{`Type` == "background"}),
+                  aes = aes(yMin = f{max(`Counts` - 1.0, 0.0)}, yMax = f{`Counts` + 1.0}),
+                  binPosition = "center") +
+    xlab("Energy [keV]") + ylab("#") +
+    ggtitle("A multi-layer plot of a histogram and scatter plot with error bars") +
+    ggsave("images/multi_layer_histogram.png")
+
+nbImage("images/multi_layer_histogram.png")
 
 nbText: """
+
+Maybe things have become a bit less confusing now.
 """
 
 nbSave
