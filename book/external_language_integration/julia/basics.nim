8bc3e01f9db3243c5b7edb13bd4a59534d575422
Regis Caillaud <35006197+Clonkk@users.noreply.github.com>
2021-11-11 17:12:48 +0100

    Update book/external_language_integration/julia/basics.nim
    Co-authored-by: Hugo Granström <5092565+HugoGranstrom@users.noreply.github.com>

--- book/external_language_integration/julia/basics.nim
+++ book/external_language_integration/julia/basics.nim
@@ -10,7 +10,7 @@ In this tutorial, we explore how to use [Nimjl](https://github.com/Clokk/nimjl) 
 
 ## What is Julia ?
 
-Julia is a dynamicalyl typed scripting language designed for high performance; it compiles to efficient native code through LLVM.
+Julia is a dynamically typed scripting language designed for high performance; it compiles to efficient native code through LLVM.
 
 Most notably, it has a strong emphasis on scientific computing and Julia Arrays type are one of the fastest multi-dimensionnal arrays - or Tensor-like - data structures out there.
 
