08df98206340bbc1041244e5d231c1f7c1bad3e4
HugoGranstrom <5092565+HugoGranstrom@users.noreply.github.com>
2022-12-31 12:43:10 +0100

    2d interpolation done

--- book/numerical_methods/interpolation.nim
+++ book/numerical_methods/interpolation.nim
@@ -17,7 +17,7 @@ We will be using [numericalnim](https://github.com/SciNim/numericalnim) for this
 """
 
 nbCode:
-  import numericalnim, ggplotnim, std/[math, sequtils]
+  import numericalnim, arraymancer, ggplotnim, std/[math, sequtils]
 
 block Part1:
   nbText: hlMd"""
@@ -94,11 +94,92 @@ It is worse where the function is changing a lot and closer to the original
 in the middle where there is less happening.  
 """
 
-nbText: hlMd"""
+block Part2:
+  nbText: hlMd"""
 ## 2D interpolation
+For 2D interpolation, `numericalnim` offers 3 methods for gridded data:
+- `newNearestNeighbour2D`
+- `newBilinearSpline`
+- `newBicubicSpline`
+
+and 2 methods for scattered data:
+
+- `newBarycentric2D`
+- `newRbf`
+The RBF method isn't unique to 2D and works for any dimension, see the next section for more on that.
+For this tutorial, we will focus on the Bicubic spline but NearestNeighbour and Bilinear spline have the same API.
+Let's first choose a suitable function to interpolate:
+$$f(x, y) = e^{-(x^2 + y^2)}$$
+This will be a Gaussian centered around `(0, 0)`, here's a heatmap showing the function:
 """
 
-nbText: hlMd"""
+  block:
+    let coords = meshgrid(arraymancer.linspace(-1.0, 1.0, 100), arraymancer.linspace(-1.0, 1.0, 100))
+    let z = exp(-1.0 * sum(coords *. coords, axis=1)).squeeze()
+    let df = toDf({"x": coords[_,0].squeeze, "y": coords[_,1].squeeze, "z": z})
+    ggplot(df, aes("x", "y", fill = "z")) +
+      geom_raster() +
+      xlim(-1, 1) +
+      ylim(-1, 1) +
+      ggsave("images/2d_interp_func.png")
+  
+  nbImage("images/2d_interp_func.png")
+
+  nbText: hlMd"""
+Now we are ready to sample our function. The inputs expected are the limits in $x$ and $y$ along
+with the function values in grid as a Tensor. So if we have a 3x3 grid of data points, the Tensor
+should have shape 3x3 as well. Because the data is known to be on a grid, you don't have to give
+all the points on it, it's enough to only provide the limits as two tuples.
+"""
+
+  nbCode:
+    proc f(x, y: float): float =
+      exp(-(x*x + y*y))
+    
+    let xlim = (-1.0, 1.0)
+    let ylim = (-1.0, 1.0)
+
+    let xs = linspace(xlim[0], xlim[1], 5)
+    let ys = linspace(ylim[0], ylim[1], 5)
+    var z = zeros[float](xs.len, ys.len)
+    for i, x in xs:
+      for j, y in ys:
+        z[i, j] = f(x, y)
+    
+  nbText: "Now we have sampled our function, let's construct the interpolator:"
+
+  nbCode:
+    let interp = newBicubicSpline(z, xlim, ylim)
+
+  nbText: "The interpolator can now be evaluated using `eval`:"
+
+  nbCode:
+    echo interp.eval(0.0, 0.0)
+
+  nbText: hlMd"""
+We can now plot the function to see how much it resembles the original function:  
+"""
+
+  block:
+    let coords = meshgrid(arraymancer.linspace(-1.0, 0.999, 100), arraymancer.linspace(-1.0, 0.999, 100))
+    var z = zeros[float](coords.shape[0])
+    for i in 0 ..< coords.shape[0]:
+      z[i] = interp.eval(coords[i, 0], coords[i, 1])
+    let df = toDf({"x": coords[_,0].squeeze, "y": coords[_,1].squeeze, "z": z})
+    ggplot(df, aes("x", "y", fill = "z")) +
+      geom_raster() +
+      xlim(-1, 1) +
+      ylim(-1, 1) +
+      ggsave("images/2d_interp_eval.png")
+    
+  nbImage("images/2d_interp_eval.png")
+
+  nbText: hlMd"""
+It looks pretty similar to the original function so I'd say it did a good job.
+"""
+
+block Part3:
+  nbText: hlMd"""
 ## ND interpolation
 
 """
